578f47599439f3a9a3221666e2b6485d8f5ed3e6
Filip <koprivec.filip@gmail.com>
2016-09-17 01:53:42 +0200

    Added file copying

--- stubgen_recursive.py
+++ stubgen_recursive.py
@@ -1,5 +1,6 @@
-import os.path
+import os
 import pkgutil
+import shutil
 import sys
 
 import mypy.stubgen
@@ -10,8 +11,10 @@ if os.path.exists(os.path.join(parent_dir, '.git')):
     # We are running from a git clone.
     sys.path.insert(0, parent_dir)
 
+package_name = sys.argv[1]
+
 try:
-    package = __import__(sys.argv[1])
+    package = __import__(package_name)
 except ImportError:
     print("Usage: python stubgen_recursive.py <module_name>")
     exit(1)
@@ -24,3 +27,11 @@ for importer, modname, ispkg in pkgutil.walk_packages(path=package.__path__, pre
 sys.argv[1:] = args
 
 mypy.stubgen.main()
+
+# copy files
+for root, dirs, files in os.walk(os.path.join("out", package_name)):
+    for file in files:
+        path_file = os.path.join(root, file)
+        _, file_path = path_file.split(os.sep, 1)
+        if not os.path.isfile(file_path):
+            shutil.copy2(path_file, file_path)
